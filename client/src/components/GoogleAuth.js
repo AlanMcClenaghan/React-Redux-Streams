0a06218d103ab1845510645217d5d6a1dec4ccd0
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-08 19:20:22 +0000

    Configured Redux dev tools.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -5,12 +5,11 @@ import { signIn, signOut } from '../actions';
 class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client
-        .init({
-          clientId:
-            '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
-          scope: 'email'
-        })
+      window.gapi.client.init({
+        clientId:
+          '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+        scope: 'email'
+      })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
 
