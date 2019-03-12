bbe9611b2a6c83fedb0ec812c4483ef596dc43f9
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-12 22:52:37 +0000

    Edit streams.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -9,11 +9,11 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
-  renderAdminBtns(stream) {
+  renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
           <button className="ui button negative">Delete</button>
         </div>
       )
@@ -34,7 +34,7 @@ class StreamList extends Component {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
-          {this.renderAdminBtns(stream)}
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
